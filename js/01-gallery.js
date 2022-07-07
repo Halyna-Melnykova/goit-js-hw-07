2a210fd3a8b164cf22c3f73a3b6976f791bae369
Halyna Melnykova <galyamelnykova@gmail.com>
2022-07-07 17:06:49 +0300

    hw7 task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// Розмітка
+
+const galleryListEl = document.querySelector('.gallery');
+const galleryArr = galleryItems.map(({ preview, original, description } = {}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+    </div>`;
+}).join('');
+
+galleryListEl.insertAdjacentHTML('afterbegin', galleryArr);
+
+// Делегування
+
+galleryListEl.addEventListener('click', selectImage);
+
+function selectImage(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+      return;
+    }
+    // console.log(event.target.dataset.source);
+    // console.log(event.target);
+    
+    // basicLightbox 
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

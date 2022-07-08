49aca339ddfa0dfc67e6fc8c24e5aa278e116dcc
Halyna Melnykova <galyamelnykova@gmail.com>
2022-07-08 16:24:18 +0300

    1-2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 // console.log(galleryItems);
 
 // Розмітка
 
-const galleryListEl = document.querySelector('.gallery');
-const galleryArr = galleryItems.map(({ preview, original, description } = {}) => {
+const galleryListEl = document.querySelector(".gallery");
+const galleryArr = galleryItems
+  .map(({ preview, original, description } = {}) => {
     return `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
       <img
@@ -17,40 +18,44 @@ const galleryArr = galleryItems.map(({ preview, original, description } = {}) =>
       />
     </a>
     </div>`;
-}).join('');
+  })
+  .join("");
 
-galleryListEl.insertAdjacentHTML('afterbegin', galleryArr);
+galleryListEl.insertAdjacentHTML("afterbegin", galleryArr);
 
 // Делегування
 
-galleryListEl.addEventListener('click', selectImage);
+let instance
+
+galleryListEl.addEventListener("click", selectImage);
 
 function selectImage(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-      return;
-    }
-    // console.log(event.target.dataset.source);
-    // console.log(event.target);
-    
-    // basicLightbox 
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  // console.log(event.target.dataset.source);
+  // console.log(event.target);
 
-    const instance = basicLightbox.create(`
+  // basicLightbox (Заміна src)
+
+  instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-
-instance.show();
+`);
+  instance.show();
+  // console.log(instance);
 }
 
+// Закриття по Esc
 
+const onEscBtnPush = event => {
+  if (event.code !== 'Escape') {
+    return;
+  }
 
+  instance.close();
+  // instance.close(() => console.log('lightbox not visible anymore'));
+  window.removeEventListener('keydown', onEscBtnPush);
+};
 
-
-
-
-
-
-
-
-
-
+window.addEventListener('keydown', onEscBtnPush);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector(".gallery");
+const galleryArr = galleryItems
+  .map(({ preview, original, description } = {}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__item" href=${original}>
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+    </div>`;
+  })
+  .join("");
+
+galleryListEl.insertAdjacentHTML("afterbegin", galleryArr);
+
+// simplelightbox
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
